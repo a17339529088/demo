4d6a507f2408c0d40602a521ac16590c75e1f295
Lzb <1795976603@qq.com>
2022-07-26 13:33:00 +0800

    自动构建测试3

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String hello1(){
-        return "hell";
+        return "hell0";
     }
 
 }
